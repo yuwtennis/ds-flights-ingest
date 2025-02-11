23f87cf6b3f74d7f4b4c4972c95b0c3f2f5d1fb5
ywatanabe <yu.w.tennis@gmail.com>
2025-02-11 19:54:19 +0900

    chore: refactored shell script

--- scripts/ingest.sh
+++ scripts/ingest.sh
@@ -8,8 +8,10 @@
 BTS="https://transtats.bts.gov/PREZIP"
 BASEURL="${BTS}/On_Time_Reporting_Carrier_On_Time_Performance_1987_present"
 ZIP_CONTENT_PREFIX="On_Time_Reporting_Carrier_On_Time_Performance_(1987_present)"
+BQ_DATASET="dsongcp"
+BQ_TABLE="flights_raw"
 
-function run() {
+function download() {
   local year=$1
   local month_start=$2
   local month_end=$3
@@ -32,6 +34,8 @@ function main() {
   local year=$(date "+%Y")
   local start=1
   local end=12
+  local project_id=$(gcloud config get core/project)
+  local gs_data_in_place_dir="gs://${project_id}-ds/flights/raw"
 
   while getopts ":y:s:e:h" opt; do
     case $opt in
@@ -59,8 +63,25 @@ function main() {
   fi
 
   echo "Downloading files for year: $year month from $start to $end."
+  download $year $start $end
 
-  run $year $start $end
+  # Upload to data-in-place
+  echo "Uploading to google cloud storage"
+  gsutil -m cp *.csv "$gs_data_in_place_dir/"
+
+  # Load to bigquery
+  for m in `seq $start $end`; do
+    ym="$(printf "%04d%02d" $year $m)"
+    csvfile="${gs_data_in_place_dir}/$ym.csv"
+
+    echo "Loading to BQ csv: $csvfile"
+    bq -project_id ${project_id} load \
+      --source_format=CSV \
+      --skip_leading_rows=1 \
+      --ignore_unknown_values \
+      --autodetect=false \
+      "${project_id}:${BQ_DATASET}.${BQ_TABLE}\$$ym" $csvfile
+  done
 }
 
 main "$@"
