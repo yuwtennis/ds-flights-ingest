cbefad5725d69542bc068a322e57c7b769b5ee4c
ywatanabe <yu.w.tennis@gmail.com>
2025-02-11 19:54:40 +0900

    chore: version constraint for client and provider

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,9 +2,10 @@ terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = "6.19.0"
+      version = "~> 6.19.0"
     }
   }
+  required_version = "~> 1.10.5"
 }
 
 provider "google" {
